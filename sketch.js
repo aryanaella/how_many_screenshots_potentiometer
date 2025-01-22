f365209f74a630c8286202a459d086ffb4e6061b
aryanaella <aryanaella@gmail.com>
2025-01-22 11:59:02 +0000

    replaced files

--- sketch.js
+++ sketch.js
@@ -1,5 +1,5 @@
+let port;
 let connectBtn;
-let slider;
 let myVal = 0;
 
 let capture;
@@ -12,55 +12,50 @@ let imageSaved = false;
 let recogniseBtn;
 let cnv;
 let hold;
-let posterization;
 
 let textRow = 50;
 
+function NavigationPreloadManager() {
+  img = loadImage()
+}
 
 function setup() {
   compressedGraphics = createGraphics(320, 240);
 
   cnv = createCanvas(800, 1000);
 
-  connectBtn = createButton('START');
+  port = createSerial();
+  connectBtn = createButton('Connect to Arduino');
   connectBtn.position(200, 200);
   connectBtn.style('font-size', '30px');
   connectBtn.style('background-color', 'yellow');
   connectBtn.mousePressed(connectBtnClick);
 
   recogniseBtn = createButton("I can't recognise myself!")
-  recogniseBtn.position(410, 5);
+  recogniseBtn.position(410, 10);
   recogniseBtn.style('font-size', '30px');
   recogniseBtn.style('background-color', 'red');
   recogniseBtn.hide();
   recogniseBtn.mousePressed(saveImage);
 
-  saveButton = createButton('save image');
-  saveButton.position(120, 680);
-  saveButton.style('font-size', '30px');
-  saveButton.style('background-color', 'pink')
-  saveButton.hide();
-  saveButton.mousePressed(downloadImage);
-
   capture = createCapture(VIDEO, {flipped:true});
   capture.size(320, 240);
   capture.hide();
-
-  slider = createSlider(0, 1100, 1);
-  slider.position(20, 15);
-  slider.size(350);
-  slider.hide();
 
   hold = createImage(320, 240);
 }
 
 function draw() {
-  myVal = slider.value();
-  
-  compressionFactor = map(myVal, 0, 1100, 0.15, 0.01);
-  compressions = map(myVal, 0, 1100, 0, 100);
+  let val = port.readUntil("\n");
+  if (val.length > 0) {
+    myVal = val;
+    console.log(myVal);
+  }
 
-  let posterization = map(myVal, 0, 1100, 15, 1.5);
+  compressionFactor = map(myVal, 0, 1100, 0.01, 0.15);
+  compressions = map(myVal, 0, 1100, 100, 0);
+
+  let posterization = map(myVal, 0, 1100, 2, 20);
   
   let compressedWidth = capture.width * compressionFactor;
   let compressedHeight = capture.height * compressionFactor;
@@ -71,39 +66,35 @@ function draw() {
   compressedGraphics.filter(POSTERIZE, posterization);
   
   if(portConnected) {
-  image(compressedGraphics, 0, 50, 400, 300);
+  image(compressedGraphics, 0, 0, 400, 300);
   recogniseBtn.show();
-  slider.show();
   }
 
-  image(hold, 0, 349, 400, 300);
+  image(hold, 0, 300, 400, 300);
 }
 
 function saveImage() {
   if(imageSaved) {
     textRow += 50;
-    if(textRow >= 1000) {
-      textRow = 50;
-      background(255);
-    }
   }
   imageSaved = true;
-  saveButton.show();
   hold = compressedGraphics.get(0, 0, compressedGraphics.width, compressedGraphics.height);
   console.log('Screenshot taken');
   textSize(20);
-  text("you couldn't recognise yourself screenshotted " + compressions + " times", 410, textRow, 400);
+  text("you couldn't recognise yourself screenshotted " + compressions + " times", 400, textRow, 400);
 
   print(posterization);
 }
 
 function connectBtnClick() {
-  portConnected = true;
-  textSize(20);
-  text('loading...', 150, 150);
-  connectBtn.hide();
-}
-
-function downloadImage() {
-  save(hold, 'screeshotted');
+  if (!port.opened()) {
+    portConnected = true;
+    port.open('Arduino', 600);
+    connectBtn.hide();
+  } else {
+    port.close();
+  }
+  if (port.close()) {
+    connectBtn.show();
+  }
 }
